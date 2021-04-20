e3633ebd68be398ee2df07a431612900c9d875ea
gk07 <gk_520@hotmail.com>
2021-04-20 18:51:46 +0800

    Add readme

---
+++ load_key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Load encrpted v2ray, git keys from private repos
+# IyBodHRwczovL2dpc3QuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0t1bmtnZy9mNzFk
+# ZjZkZjM1OTEwYWNkNzQ2ZTlhOWFlMDBjMTY5Ni9yYXcvOTNiNjZmMDc1NGExZmEx
+# NWYyMzQ3OGY2ZjVjZjhiZmQxMWZodHRwczovL2dpc3QuZ2l0aHVidXNlcmNvbnRl
+# bnQuY29tL0t1bmtnZy9mNzFkZjZkZjM1OTEwYWNkNzQ2ZTlhOWFlMDBjMTY5Ni9y
+# YXcvOTNiNjZmMDc1NGExZmExNWYyMzQ3OGY2ZjVjZjhiZmQxMWZkYzJiZS9yYW5k
+# b20udHh0ZGMyYmUvcmFuZG9tLnR4dAo=
+
+# LTI1Ni1jYmMgLWl0ZXIgMjIgLXBia2RmMgo=
+
+export keys_repo="https://github.com/Kunkgg/arch_setup_keys.git"
